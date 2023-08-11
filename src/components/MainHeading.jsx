462653bc9d08b9fae59f364e9bf7ab21ee6bea7e
Zohaib Ahmad <zohaibsattar1039@gmail.com>
2023-08-12 01:55:28 +0500

    Add media queries for responsive design

--- src/components/MainHeading.jsx
+++ src/components/MainHeading.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 
 const MainHeading = () => {
 
     return (
 
+        <NavLink className="navlink" to={"/"}>
         <div className="main-heading">
             <h1>Meme Generator App</h1>
         </div>
+        </NavLink>
 
     )
 
