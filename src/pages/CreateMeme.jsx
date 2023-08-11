ea3e65f33297386657d1ecf3351c7e6bf616268c
Zohaib Ahmad <zohaibsattar1039@gmail.com>
2023-08-12 01:35:30 +0500

    Add functionality of saving meme to device

--- src/pages/CreateMeme.jsx
+++ src/pages/CreateMeme.jsx
@@ -1,24 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 import MemeEditor from "../components/MemeEditor";
 import MemeText from "../components/MemeText";
+import { exportComponentAsJPEG } from "react-component-export-image";
 
 
 const CreateMeme = () => {
 
     const [textCount, setTextCount] = useState(0);
+    const memeRef = useRef();
 
     const [searchParams] = useSearchParams();
     const imgUrl = searchParams.get("url");
 
-    const addNewTextOnScreen=()=>{
-        setTextCount(textCount+1);
+    const addNewTextOnScreen = () => {
+        setTextCount(textCount + 1);
+    }
+    const saveImageToDevice = () => {
+        exportComponentAsJPEG(memeRef);
     }
 
     return (
         <div className="create-meme-page">
 
-                <MemeEditor imgUrl={imgUrl} />
+            <div ref={memeRef} className="download-div">
+                <MemeEditor memeRef={memeRef} imgUrl={imgUrl} />
 
                 {
                     Array(textCount).fill(0).map((element) => {
@@ -26,15 +32,17 @@ const CreateMeme = () => {
                     })
                 }
 
-                <div className="buttons">
-                    <button onClick={addNewTextOnScreen}>Add Text</button>
-                    <button>Save</button>
-                </div>
-
             </div>
-            )
+
+            <div className="buttons">
+                <button onClick={addNewTextOnScreen}>Add Text</button>
+                <button onClick={saveImageToDevice}>Save</button>
+            </div>
+
+        </div>
+    )
 
 }
 
 
-            export default CreateMeme;
+export default CreateMeme;
