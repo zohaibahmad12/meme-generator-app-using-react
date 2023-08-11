d8e0fffe071dac271e7322567ae5bffaa739c228
Zohaib Ahmad <zohaibsattar1039@gmail.com>
2023-08-12 01:03:58 +0500

    Adding text on meme functionality implemented

--- src/components/MemeText.jsx
+++ src/components/MemeText.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 
-const MemeText=()=>{
 
+const MemeText = () => {
+
+    const [isEditable, setIsEditable] = useState(false);
+    const [input, setInput] = useState("");
+
+    const handleMemeTextToggle = () => {
+        setIsEditable(!isEditable);
+    }
+
+    const handleInputChange = (event) => {
+        setInput(event.target.value);
+    }
+
+    return (
+        <Draggable>
+            <div className="meme-text">
+                {
+                    (isEditable) ? <input placeholder="Add Text..." onDoubleClick={handleMemeTextToggle} type="text" value={input} onChange={handleInputChange} /> :
+                       <p onDoubleClick={handleMemeTextToggle}>{(input)? input: "Double Click To Edit"}</p>
+                }
+            </div>
+        </Draggable>
+    )
 }
 
 export default MemeText;
--- src/pages/CreateMeme.jsx
+++ src/pages/CreateMeme.jsx
@@ -1,33 +1,40 @@
 import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MemeEditor from "../components/MemeEditor";
+import MemeText from "../components/MemeText";
 
 
 const CreateMeme = () => {
 
-    const [textCount,setTextCount]=useState(0);
-    
+    const [textCount, setTextCount] = useState(0);
+
     const [searchParams] = useSearchParams();
     const imgUrl = searchParams.get("url");
+
+    const addNewTextOnScreen=()=>{
+        setTextCount(textCount+1);
+    }
 
     return (
         <div className="create-meme-page">
 
-            <MemeEditor imgUrl={imgUrl} />
+                <MemeEditor imgUrl={imgUrl} />
 
-            <div className="buttons">
-                <button>Add Text</button>
-                <button>Save</button>
+                {
+                    Array(textCount).fill(0).map((element) => {
+                        return <MemeText />
+                    })
+                }
+
+                <div className="buttons">
+                    <button onClick={addNewTextOnScreen}>Add Text</button>
+                    <button>Save</button>
+                </div>
+
             </div>
-{
-            Array(textCount).fill(0).map((element)=>{
-                return <h1>Hello</h1>
-            })
-}
-        </div>
-    )
+            )
 
 }
 
 
-export default CreateMeme;
+            export default CreateMeme;
